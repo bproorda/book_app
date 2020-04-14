6a4928d3e8f0b6bc701fc14e55f9296202d3da0b
Stacey Teltser <stacey.teltser@gmail.com>
2020-04-14 17:44:17 -0500

    started building error handler stuff

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const superagent = require('superagent');
+// const pg = require('pg');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -20,13 +20,24 @@ app.get('/show', (request, response) => {
   // console.log('/show', request.query);
   response.render('pages/searches/show');
 });
+
 app.get('/searches', (request, response) => {
   console.log('/searches', request.query);
   response.render('pages/searches/searches', {message1: 'The Library is closed due to PLAGUE!!!'});
-})
+});
+
+app.get('*', (req, res) => res.status(404).send('This route does not exist'));
+
 
 const cors = require('cors');
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
+function errorHandler(err, response) {
+  let viewModel = {
+    error: err,
+  };
+  response.render('pages/error', viewModel);
+}

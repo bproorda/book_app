1a2abc8b7c616b11ccb99e98133978c87c250e61
Stacey Teltser <stacey.teltser@gmail.com>
2020-04-14 10:12:13 -0500

    scaffolding started to build out and connected to loclocal host

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
